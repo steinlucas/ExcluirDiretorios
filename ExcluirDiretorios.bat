a4776fc5d61a4cd9d0df5b1eaffd8e471b49b4ba
Lucas Stein <66976832+steinlucas@users.noreply.github.com>
2023-07-24 13:48:09 -0300

    Update ExcluirDiretorios.bat

--- ExcluirDiretorios.bat
+++ ExcluirDiretorios.bat
@@ -1,8 +1,8 @@
 @echo off
 
 FOR /D %%c in (*) DO (
-rd /s /q %cd%\%%c\Dados\BinarioPlano
-rd /s /q %cd%\%%c\Dados\BinarioEfetivacao
+    rd /s /q %cd%\%%c\Dados\BinarioPlano
+    rd /s /q %cd%\%%c\Dados\BinarioEfetivacao
 )
 
 pause
